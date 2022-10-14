c71263ce07fd43c6129d7f4762f00ce7b980524b
allldo <dotan2.maksim@gmail.com>
2022-10-14 12:16:52 +0600

    quickview add to wishlist add to cart

--- api/views.py
+++ api/views.py
@@ -5,17 +5,19 @@ from rest_framework.response import Response
 
 from shop.service import get_customer
 from .serializers import ProductSerializer
-from shop.models import Product
+from shop.models import Product, ProductCartItem
 
 
 @api_view(['GET'])
 def get_product_info(request: WSGIRequest):
     product_pk = request.GET.get('product_pk')
     product = get_object_or_404(Product, pk=product_pk)
+    customer = get_customer(request)
     serialized_product = ProductSerializer(product).data
-    wishlisted = True if product in get_customer(request).wishlist.all() else False
+    wishlisted = True if product in customer.wishlist.all() else False
+    in_cart = True if ProductCartItem.objects.filter(customer=customer, product_id=product_pk).exists() else False
     return Response({
-        'product': serialized_product, 'wishlisted': wishlisted
+        'product': serialized_product, 'wishlisted': wishlisted, 'in_cart': in_cart
     })
 
 
